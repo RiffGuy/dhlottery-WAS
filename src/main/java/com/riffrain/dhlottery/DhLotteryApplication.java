15441a1fb4d0eeabb02b95b7e42909a118a03631
RiffGuy <jins3569@gmail.com>
2022-08-27 19:51:27 +0900

    - 프로젝트 초기 세팅 - 동행복권 계정 확인 기능 생성

---
+++ src/main/java/com/riffrain/dhlottery/DhLotteryApplication.java
@@ -0,0 +1,13 @@
+package com.riffrain.dhlottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhLotteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhLotteryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riffrain/dhlottery/DhLotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riffrain.dhlottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhLotteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
